43c86e46ae89b169ad55f9bb67671ada5a8c85e4
Lee <lee.coakley@mycit.ie>
2013-04-03 00:58:54 +0100

    GenericPanel placeholder

---
+++ overwatch/src/overwatch/gui/GenericPanel.java
@@ -0,0 +1,25 @@
+
+
+
+package overwatch.gui;
+
+import javax.swing.JPanel;
+
+
+
+
+
+/**
+ * Generic panel with a left-side search panel and right-side main panel.
+ * @author Lee Coakley
+ *
+ */
+
+
+
+
+
+public class GenericPanel extends JPanel
+{
+	
+}
