0967dee5cab832384ba990b27acc62d453e02567
Lee <lee.coakley@mycit.ie>
2013-04-02 17:41:42 +0100

    PersonnelPicker: placeholder

---
+++ overwatch/src/overwatch/gui/PersonnelPicker.java
@@ -0,0 +1,24 @@
+
+
+
+package overwatch.gui;
+
+
+
+
+/**
+ * A modal dialogue for picking personnel from the database.
+ * 
+ * @author  Lee Coakley
+ * @version 1
+ * @see 	SearchPanel
+ */
+
+
+
+
+
+public class PersonnelPicker
+{
+	
+}
