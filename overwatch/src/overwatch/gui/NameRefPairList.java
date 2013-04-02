cd30eb5201d1721738a6257da47ff220d9f882ee
Lee <lee.coakley@mycit.ie>
2013-04-02 16:29:54 +0100

    Easier NameRefPairList/EnhancedResultSet interoperability

--- overwatch/src/overwatch/gui/NameRefPairList.java
+++ overwatch/src/overwatch/gui/NameRefPairList.java
@@ -8,10 +8,35 @@ import java.util.ArrayList;
 
 
 
+/**
+ * Nicer version of ArrayList<NameRefPair<T>>.
+ * 
+ * @author  Lee Coakley
+ * @version 1
+ */
+
+
+
 
 public class NameRefPairList<T> extends ArrayList<NameRefPair<T>>
 {
+	
+	public NameRefPairList( T[] refs, String[] names )
+	{
+		if (refs.length != names.length)
+			throw new RuntimeException( "Array lengths do not match." );
+		
+		for (int i=0; i<refs.length; i++) {
+			addPair( refs[i], names[i] );
+		}
+	}
+	
+	
+	
+	
+	
 	public void addPair( T ref, String name ) {
 		this.add( new NameRefPair<T>( ref, name ));
 	}
+	
 }
