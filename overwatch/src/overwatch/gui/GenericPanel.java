478a9002ab0c64ac25481c795cabf7843c251af5
Lee <lee.coakley@mycit.ie>
2013-04-03 14:57:43 +0100

    GenericPanel: genericised addToMain/addToSub

--- overwatch/src/overwatch/gui/GenericPanel.java
+++ overwatch/src/overwatch/gui/GenericPanel.java
@@ -65,16 +65,18 @@ public class GenericPanel<T> extends JPanel
 	
 	
 	
-	public void addToMain( Component comp, String layoutParams ) {
+	public <C extends Component> C addToMain( C comp, String layoutParams ) {
 		mainPanel.add( comp, layoutParams );
+		return comp;
 	}
 	
 	
 	
 	
 	
-	public void addToSub( Component comp, String layoutParams ) {
+	public <C extends Component> C addToSub( C comp, String layoutParams ) {
 		subPanel.add( comp, layoutParams );
+		return comp;
 	}
 	
 	
