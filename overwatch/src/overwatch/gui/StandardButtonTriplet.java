757a6ad1c10393ba17a7e0fac3086a524304a536
Lee <lee.coakley@mycit.ie>
2013-04-03 14:29:30 +0100

    Added StandardButtonTriplet (new/save/load)

---
+++ overwatch/src/overwatch/gui/StandardButtonTriplet.java
@@ -0,0 +1,29 @@
+
+
+
+package overwatch.gui;
+
+import javax.swing.JButton;
+
+
+
+
+
+public class StandardButtonTriplet
+{
+	public final JButton addNew;
+	public final JButton save;
+	public final JButton delete;
+	
+		
+	
+	
+	
+	public StandardButtonTriplet( JButton addNew, JButton save, JButton delete )
+	{
+		this.addNew = addNew;
+		this.save   = save;
+		this.delete = delete;
+	}	
+}
+
