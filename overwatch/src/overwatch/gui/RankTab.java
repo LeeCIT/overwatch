95357d1eac7c2261852df70301bd860626b49861
John <john.murphy7@mycit.ie>
2013-04-03 17:58:02 +0100

    Rank tab design complete.

---
+++ overwatch/src/overwatch/gui/RankTab.java
@@ -0,0 +1,39 @@
+package overwatch.gui;
+
+import javax.swing.JFrame;
+
+public class RankTab extends GenericPanelButtoned<Integer>{
+	
+	LabelFieldPair name;
+	LabelFieldPair privileges;
+	
+	
+	
+	
+	public RankTab()
+	{
+		super( "Rank", "Details" );		
+		setupComponents();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Internals
+	/////////////////////////////////////////////////////////////////////////
+	
+	private void setupComponents()
+	{
+		name    = addLabelledField( "Name:");
+		privileges = addLabelledField("Privileges:");
+	}
+		
+		
+		//Test
+		public static void main(String[] args)
+		{
+			JFrame frame = new JFrame();
+			frame.add(new RankTab());
+			frame.pack();
+			frame.setVisible(true);		
+		}
+
+}
