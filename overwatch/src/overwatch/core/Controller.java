756db67befd963daec9fb8bddab8037330c4aa4b
Lee <lee.coakley@mycit.ie>
2013-04-04 17:21:23 +0100

    Added core.Controller.attachLogicControllers( Gui )

--- overwatch/src/overwatch/core/Controller.java
+++ overwatch/src/overwatch/core/Controller.java
@@ -9,5 +9,15 @@ package overwatch.core;
 
 public class Controller
 {
-	// Placeholder
+	
+	/**
+	 * Plug the logic controllers into the GUI components.
+	 * @param gui Gui instance.
+	 */
+	public static void attachLogicControllers( Gui gui )
+	{
+		PersonnelLogic.attachEvents( gui.personnelTab );
+		// TODO: insert other controllers here
+	}
+	
 }
