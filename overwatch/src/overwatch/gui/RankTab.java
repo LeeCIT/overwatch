25a4edc021175f965903a5b729c35a8f0579c658
John <john.murphy7@mycit.ie>
2013-04-04 20:15:31 +0100

    Resolved merge

--- overwatch/src/overwatch/gui/RankTab.java
+++ overwatch/src/overwatch/gui/RankTab.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package overwatch.gui;
 
 import javax.swing.JFrame;
@@ -82,44 +81,4 @@ public class RankTab extends GenericPanelButtoned<Integer>{
 	}
 
 }
-=======
-package overwatch.gui;
 
-import javax.swing.JFrame;
-
-public class RankTab extends GenericPanelButtoned<Integer>{
-	
-	LabelFieldPair name;
-	LabelFieldPair privileges;
-	
-	
-	
-	
-	public RankTab()
-	{
-		super( "Rank", "Details" );		
-		setupComponents();
-	}
-	
-	///////////////////////////////////////////////////////////////////////////
-	// Internals
-	/////////////////////////////////////////////////////////////////////////
-	
-	private void setupComponents()
-	{
-		name    = addLabelledField( "Name:");
-		privileges = addLabelledField("Privileges:");
-	}
-		
-		
-		//Test
-		public static void main(String[] args)
-		{
-			JFrame frame = new JFrame();
-			frame.add(new RankTab());
-			frame.pack();
-			frame.setVisible(true);		
-		}
-
-}
->>>>>>> refs/heads/master
