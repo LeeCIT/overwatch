37a9549d3addfbc15bc9519d5c27922a60f182b8
John <john.murphy7@mycit.ie>
2013-04-05 18:01:36 +0100

    Code cleanup

--- overwatch/src/overwatch/controllers/RankLogic.java
+++ overwatch/src/overwatch/controllers/RankLogic.java
@@ -1,13 +1,7 @@
 package overwatch.controllers;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import javax.swing.JFrame;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
+import java.awt.event.*;
+import javax.swing.event.*;
 import overwatch.db.Database;
-import overwatch.db.EnhancedResultSet;
 import overwatch.gui.tabs.RankTab;
 
 
