1a74e917ffa00a44dd9833065c3bb831d7919eff
Lee <lee.coakley@mycit.ie>
2013-04-05 23:28:47 +0100

    Controller: Testing gui.addTabChangeListener

--- overwatch/src/overwatch/core/Controller.java
+++ overwatch/src/overwatch/core/Controller.java
@@ -3,6 +3,8 @@
 
 package overwatch.core;
 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import overwatch.controllers.*;
 
 
@@ -21,13 +23,20 @@ public class Controller
 	 * Plug the logic controllers into the GUI components.
 	 * @param gui Gui instance.
 	 */
-	public static void attachLogicControllers( Gui gui )
+	public static void attachLogicControllers( final Gui gui )
 	{
 		new PersonnelLogic( gui.personnelTab );
 		new RankLogic     ( gui.rankTab      );
 		new VehicleLogic  ( gui.vehicleTab   );
 		new SupplyLogic   ( gui.supplyTab    );
 		// TODO: insert other controllers here
+		
+		
+		gui.addTabChangeListener( new ChangeListener() {
+			public void stateChanged( ChangeEvent e ) {
+				System.out.println( gui.tabPane.getSelectedComponent() );	
+			}
+		});
 	}
 	
 }
