dd1dc67bd9c8c3dc50049ddf5185f7bf9b7eb42f
Lee <lee.coakley@mycit.ie>
2013-04-08 22:26:47 +0100

    NameRefPair: Removed redundant get/set

--- overwatch/src/overwatch/gui/NameRefPair.java
+++ overwatch/src/overwatch/gui/NameRefPair.java
@@ -13,50 +13,26 @@ package overwatch.gui;
  * Often you don't want the default toString() to be what is displayed.
  * 
  * @author Lee Coakley
- * @version 1
+ * @version 2
  */
 
 
 
 
 
-public class NameRefPair<Type>
+public class NameRefPair<T>
 {
-	private String name;
-	private Type   ref;
+	public String name;
+	public T      ref;
 	
 	
 	
 	
 	
-	public NameRefPair( Type ref, String displayedName )
+	public NameRefPair( T ref, String displayedName )
 	{
 		this.ref  = ref;
 		this.name = displayedName;
-	}
-
-	
-	
-	
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName( String name ) {
-		this.name = name;
-	}
-	
-	
-	
-	
-	
-	public Type getRef() {
-		return ref;
-	}
-
-	public void setRef( Type ref ) {
-		this.ref = ref;
 	}
 	
 	
@@ -64,6 +40,6 @@ public class NameRefPair<Type>
 	
 	
 	public String toString() {
-		return getName();
+		return name;
 	}
 }
