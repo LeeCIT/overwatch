02c29468947fef01393fa16490e3942184ca8b28
John <john.murphy7@mycit.ie>
2013-04-09 21:35:13 +0100

    SquadLogic: Added simple logic

---
+++ overwatch/src/overwatch/controllers/SquadLogic.java
@@ -0,0 +1,104 @@
+package overwatch.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+import overwatch.core.Gui;
+import overwatch.gui.tabs.SquadTab;
+
+
+
+
+/**
+ * The squadTabLogic
+ * @author  John Murphy
+ *
+ */
+
+
+
+
+public class SquadLogic extends TabController{
+	
+	
+	private final SquadTab tab;
+	
+	public SquadLogic(SquadTab tab)
+	{
+		this.tab = tab;
+		attatchEvents();
+		setupTabChangeActions();
+	}
+	
+	
+	
+	
+	public void respondToTabSelect(){
+		
+	}
+	
+	
+	
+	public JPanel getTab(){
+		return tab;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Internals
+	/////////////////////////////////////////////////////////////////////////
+	
+	
+	
+	private void attatchEvents(){
+		setUpButtonActions();
+	}
+	
+	
+	
+	private void setUpButtonActions(){
+		
+		tab.addNewListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("New");
+			}
+		});
+		
+		tab.addSaveListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Save");
+			}
+		});
+		
+		tab.addDeleteListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Delete");				
+			}
+		});
+	}
+	
+	
+	
+	
+	private void setupTabChangeActions() {
+		Gui.getCurrentInstance().addTabSelectNotify(this);
+	}
+	
+	
+	
+	
+
+}
