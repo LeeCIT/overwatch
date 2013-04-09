542a79675e38f2ecdba7dbb5b487ecc920c6c8c7
Lee <lee.coakley@mycit.ie>
2013-04-09 13:43:01 +0100

    Vehicles: Added create() and delete()

--- overwatch/src/overwatch/db/Vehicles.java
+++ overwatch/src/overwatch/db/Vehicles.java
@@ -11,7 +11,7 @@ package overwatch.db;
  * Database <-> Vehicle interactions
  * 
  * @author  Lee Coakley
- * @version 1
+ * @version 2
  */
 
 
@@ -35,6 +35,56 @@ public class Vehicles
 		);
 		
 		return ! ers.isEmpty();
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Create a new vehicle.
+	 * @return Integer vehicleNo
+	 */
+	public static Integer create()
+	{	
+		Integer vehicleNo = null;
+		
+		Database.lockWrite( "Vehicles" );
+		
+		try {
+			 vehicleNo = Database.queryInt(
+				"select max(vehicleNo)+1 " +
+				"from Vehicles;"
+			);
+			
+			Database.update( 
+				"insert into Vehicles values (" +
+				    vehicleNo + ", " +
+					"'new vehicle #" + vehicleNo + "'," +
+					"null" +
+				");"
+			);
+		}
+		finally {
+			Database.unlock();
+		}
+		
+		return vehicleNo;
+	}
+	
+	
+	
+	
+	
+	public static void delete( Integer vehicleNo )
+	{
+		// TODO: check if vehicle is in use in a squad, and throw an exception or show a warning message if so
+		// TODO: create Squads.usesVehicle( squadNo, vehicleNo )
+		
+		Database.update( 
+			"delete from Vehicles " +
+			"where vehicleNo =    " + vehicleNo + ";"
+		);
 	}
 	
 }
