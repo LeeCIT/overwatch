dd98608d69fc104688c800fa9857664d7bf36a18
Lee <lee.coakley@mycit.ie>
2013-04-09 16:48:23 +0100

    Revert "DatabaseConstraints: Updated to use generics"
    This reverts commit dc238742c53758b57317e4c925473c2df3fbf840.

--- overwatch/src/overwatch/db/DatabaseConstraints.java
+++ overwatch/src/overwatch/db/DatabaseConstraints.java
@@ -78,14 +78,14 @@ public class DatabaseConstraints
 	 */
 	public static boolean rankExists( String rankName )
 	{
-		Integer ranks = Database.querySingle( Integer.class,
+		Integer[] ranks = Database.queryInts(
 			"select rankNo " +
 			"from Ranks " +
 			"where name = '" + rankName + "' " +
 			"limit 1;"
 		);
 		
-		return ranks != null;
+		return (ranks.length != 0);
 	}
 	
 	
@@ -99,14 +99,14 @@ public class DatabaseConstraints
 	 */
 	public static boolean personExists( String name )
 	{
-		Integer p = Database.querySingle( Integer.class,
+		Integer[] personnelName = Database.queryInts(
 			"SELECT personNo " +
 			"from Personnel  " +
 			"where name = '" + name + "' " +
 			"limit 1;"
 		);
 	
-		return (p != null);
+		return (personnelName.length != 0);
 	}
 	
 
