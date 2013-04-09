e212fa4baa033e3e448d2e5edbf680f372e0a8fd
Lee <lee.coakley@mycit.ie>
2013-04-09 22:39:12 +0100

    Common.createWithUnique now tries a finite number of times

--- overwatch/src/overwatch/db/Common.java
+++ overwatch/src/overwatch/db/Common.java
@@ -36,23 +36,27 @@ public class Common
 	 * @param table Table to apply the insert to
 	 * @param namePrefix What to prepend the unique name with, eg "vehicle"
 	 * @param values Values you would put in the SQL query values() part.  The string "<?>" will be replaced with a random unique string.
+	 * @throws DatabaseException Thrown if it fails.
 	 */
 	public static void createWithUnique( String table, String...values )
 	{
+		int maxRetries = 5;
+		
 		String pre   = "insert into " + table + " ";
 		String vals  = "values(" + Util.concatWithCommas(values) + ");";
 		String query = pre + vals;
 		
-		for (;;) {
+		for (int r=0; r<maxRetries; r++) {
 			try {
 				Database.update(  query.replaceAll("\\<\\?\\>",randomNamePart())  );
 				return;
 			}
 			catch (DatabaseException ex) {
-				ex.printStackTrace();
-				System.exit( 1 );
+				System.out.println( "<debug> createWithUnique failed, retrying" ); // TODO remove debug
 			}
 		}
+		
+		throw new DatabaseException( "createWithUnique failed after " + maxRetries + " attempts." );		
 	}
 	
 	
