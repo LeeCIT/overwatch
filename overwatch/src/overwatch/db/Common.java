04a552019f7000263f07c81dc6f50d2be9b6ae6e
Lee <lee.coakley@mycit.ie>
2013-04-09 22:20:44 +0100

    Added db.Common.createWithUnique, as discussed

--- overwatch/src/overwatch/db/Common.java
+++ overwatch/src/overwatch/db/Common.java
@@ -13,7 +13,7 @@ import overwatch.util.Util;
  * Functions which are applicable to multiple types of data.
  * 
  * @author  Lee Coakley
- * @version 1
+ * @version 2
  */
 
 
@@ -23,13 +23,37 @@ import overwatch.util.Util;
 public class Common
 {
 	public static String randomNamePart() {
-		return "[" + Util.randomAlphaString( 12 ) + "]";
+		return "[" + Util.randomAlphaString( 8 ) + "]";
 	}
 	
 	
 	
 	
 	
+	/**
+	 * Unique creator for ranks, personnel etc
+	 * The table must use autoIncrement values.
+	 * @param table Table to apply the insert to
+	 * @param namePrefix What to prepend the unique name with, eg "vehicle"
+	 * @param values Values you would put in the SQL query values() part.  The string "<?>" will be replaced with a generated unique string.
+	 */
+	public static void createWithUnique( String table, String...values )
+	{
+		String pre   = "insert into " + table + " ";
+		String vals  = "values(" + Util.concatWithCommas(values) + ");";
+		String query = pre + vals;
+		
+		for (;;) {
+			try {
+				Database.update(  query.replace("<\\?>",randomNamePart())  );
+				return;
+			}
+			catch (DatabaseException ex) {
+				ex.printStackTrace();
+				System.exit( 1 );
+			}
+		}
+	}
 	
 	
 	
