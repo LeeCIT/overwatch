d2a87801fb2526e1bd7c070567425f38e079c070
John <john.murphy7@mycit.ie>
2013-04-09 22:07:24 +0100

    SquadLogic: All text fields load the correct information

--- overwatch/src/overwatch/controllers/SquadLogic.java
+++ overwatch/src/overwatch/controllers/SquadLogic.java
@@ -4,9 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import overwatch.core.Gui;
 import overwatch.db.Database;
+import overwatch.db.EnhancedResultSet;
 import overwatch.gui.tabs.SquadTab;
 
 
@@ -66,6 +69,7 @@ public class SquadLogic extends TabController{
 	
 	private void attatchEvents(){
 		setUpButtonActions();
+		setupListSelectActions();
 	}
 	
 	
@@ -94,17 +98,71 @@ public class SquadLogic extends TabController{
 	
 	
 	
+	private void setupListSelectActions(){
+		tab.addSearchPanelListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				loadSubPanels();
+				populateFields(tab.getSelectedItem());
+			}
+		});
+	}
+	
+	
+	
+	
+	private void populateSquadsList(){
+		populateFields(null);
+		
+		tab.setSearchableItems(
+		Database.queryKeyNamePairs("Squads", "squadNo", "squadName", Integer[].class)
+		);
+	}
+	
+	
+	
+	
+	private void loadSubPanels(){
+		//TODO
+	}
+	
+	
+	
+	
+	private void populateFields(Integer squadNo)
+	{
+		if(squadNo == null)
+		{
+			tab.setEnableFieldsAndButtons( false );
+			tab.clearFields();
+			return;
+		}
+		else{
+			tab.setEnableFieldsAndButtons( true );
+		}
+		
+		EnhancedResultSet ers = Database.query(
+				"SELECT s.squadNo, squadName, p.name AS name     " +
+			    "FROM Squads s, SquadCommanders sq, Personnel p     " +
+			    "WHERE s.squadNo =  " + squadNo + " " +
+			    "AND p.personNo = sq.personNo;"
+			);
+		
+		
+		tab.number.field.setText( "" 	+ ers.getElemAs( "squadNo", 	Integer.class ));
+		tab.name  .field.setText(      	  ers.getElemAs( "squadName",   String .class ));
+		tab.commander.field.setText( "" + ers.getElemAs( "name",    	Integer.class ));		
+	}
+	
+	
+	
+	
 	private void setupTabChangeActions() {
 		Gui.getCurrentInstance().addTabSelectNotify(this);
 	}
 	
 	
 	
-	private void populateSquadsList(){
-		tab.setSearchableItems(
-		Database.queryKeyNamePairs("Squads", "squadNo", "squadName", Integer[].class)
-		);
-	}
+	
 	
 	
 	
