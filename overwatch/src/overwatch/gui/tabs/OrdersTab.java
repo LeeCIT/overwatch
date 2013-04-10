503c536fb74340988f72854a534ad1f7c9e36005
Lee <lee.coakley@mycit.ie>
2013-04-10 21:31:52 +0100

    OrdersTab: Placeholder

---
+++ overwatch/src/overwatch/gui/tabs/OrdersTab.java
@@ -0,0 +1,22 @@
+
+
+
+package overwatch.gui.tabs;
+
+import overwatch.gui.GenericPanel;
+
+
+
+
+
+public class OrdersTab extends GenericPanel<Integer>
+{
+
+	public OrdersTab()
+	{
+		super( "Outstanding Orders" );
+		
+		// TODO
+	}
+	
+}
