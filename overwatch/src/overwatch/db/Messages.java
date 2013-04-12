dfc4d9ca9dbfba5126f7e04717d50280e0cf72eb
Lee <lee.coakley@mycit.ie>
2013-04-12 17:23:11 +0100

    Added db.Messages.create()

--- overwatch/src/overwatch/db/Messages.java
+++ overwatch/src/overwatch/db/Messages.java
@@ -9,5 +9,27 @@ package overwatch.db;
 
 public class Messages
 {
+
+	/**
+	 * Create a new message.
+	 * The returned integer is guaranteed to be the messageNo for the given message.
+	 * @param subject
+	 * @param body
+	 * @param sentBy
+	 * @param sentTo
+	 * @return
+	 */
+	public static Integer create( String subject, String body, Integer sentBy, Integer sentTo )
+	{
+		return Common.createWithUniqueLockingAutoInc(
+			"Messages",
+			"default",
+			"default",
+			"'" + subject + "'",
+			"'" + body    + "'",
+			""  + sentBy,
+			""  + sentTo
+		);
+	}
 	
 }
