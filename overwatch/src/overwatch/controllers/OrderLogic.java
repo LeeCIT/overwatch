2451be1f77cafa23c944f149565da4776baf1702
Lee <lee.coakley@mycit.ie>
2013-04-12 17:07:11 +0100

    OrderLogic: Added various methods

--- overwatch/src/overwatch/controllers/OrderLogic.java
+++ overwatch/src/overwatch/controllers/OrderLogic.java
@@ -3,6 +3,12 @@
 
 package overwatch.controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import overwatch.core.Gui;
+import overwatch.gui.tabs.OrderTab;
+
 
 
 
@@ -19,7 +25,158 @@ package overwatch.controllers;
 
 
 
-public class OrderLogic
+public class OrderLogic extends TabController
 {
+	private final OrderTab tab;
+	
+	
+	
+	
+	
+	/**
+	 * Plug the GUI tab into the controller.
+	 * @param tab
+	 */
+	public OrderLogic( OrderTab tab )
+	{
+		this.tab = tab;
+		
+		attachEvents();
+	}
+	
+	
+	
+	
+	
+	public JPanel getTab() {
+		return tab;
+	}
+	
+	
+	
+	
+	
+	public void respondToTabSelect() {	
+		populateLists();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Internals
+	/////////////////////////////////////////////////////////////////////////
+	
+	
+	
+	private void doCreateOrder()
+	{
+		// TODO
+		System.out.println( "create" );
+	}
+	
+	
+	
+	
+	
+	private void doMarkAsDone()
+	{
+		// TODO Personnel save
+		System.out.println( "mark as done" );
+	}
+	
+	
+	
+	
+	
+	private void populateLists()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	private void populateMessagePanel( Integer orderNo )
+	{
+		
+	}
+	
+	
+	
+	
+	
+	private void attachEvents()
+	{
+		setupTabChangeActions();
+		setupSelectActions   ();
+		setupButtonActions   ();
+	}
+	
+	
+	
+	
+	
+	private void setupTabChangeActions() {
+		Gui.getCurrentInstance().addTabSelectNotify( this );	
+	}
+	
+	
+	
+	
+	
+	private void setupSelectActions()
+	{
+		
+	}
+	
+	
+	
+	
+	
+	private void setupButtonActions()
+	{
+		tab.addCreateOrderListener( new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {
+				doMarkAsDone();
+			}
+		});
+		
+		
+		tab.addMarkAsDoneListener( new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {
+				doMarkAsDone();
+			}
+		});
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
