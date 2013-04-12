0f2c4736ba8367c21c677b5a7b82582a9fc80a6b
Lee <lee.coakley@mycit.ie>
2013-04-12 19:52:26 +0100

    Added Util.DateSys

---
+++ overwatch/src/overwatch/util/DateSys.java
@@ -0,0 +1,112 @@
+
+
+
+package overwatch.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+
+
+/**
+ * Provides a simple means of manipulating dates.
+ * 
+ * @author Lee Coakley
+ */
+
+
+
+
+
+public class DateSys
+{
+	private static SimpleDateFormat dateFormat = new SimpleDateFormat( "dd/MM/yyyy" );
+	
+	
+	
+	
+
+	public static boolean inInclusiveRange( Date compare, Date start, Date end ) {
+		return onOrAfter ( compare, start )
+			&& onOrBefore( compare, end   );
+	}
+	
+	
+	
+	
+	
+	public static boolean onOrBefore( Date compare, Date reference ) {
+		return (compare.getTime() <= reference.getTime());
+	}
+	
+	
+	
+	
+	
+	public static boolean onOrAfter( Date compare, Date reference ) {
+		return (compare.getTime() >= reference.getTime());
+	}
+	
+	
+	
+	
+	
+	public static String format( Date date ) {
+		return dateFormat.format( date );
+	}
+	
+	
+	
+	
+	
+	public static Date parse( String dateString ) throws ParseException {
+		return dateFormat.parse( dateString );
+	}
+	
+	
+	
+	
+	
+	public static boolean isDateValid( String dateString )
+	{
+		try {
+			dateFormat.parse( dateString );
+			return true; // Will not be reached if parse throws an exception
+		}
+		catch (ParseException ex) {
+			return false;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
