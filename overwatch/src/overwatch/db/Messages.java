638773da26719d85fe44fe4c81e8925c2deeecb4
Lee <lee.coakley@mycit.ie>
2013-04-16 13:18:41 +0100

    Messages: Switched to EnhancedPreparedStatement

--- overwatch/src/overwatch/db/Messages.java
+++ overwatch/src/overwatch/db/Messages.java
@@ -21,15 +21,31 @@ public class Messages
 	 */
 	public static Integer create( String subject, String body, Integer sentBy, Integer sentTo )
 	{
-		return Common.createWithUniqueLockingAutoInc(
-			"Messages",
-			"default",
-			"default",
-			"'" + subject + "'",
-			"'" + body    + "'",
-			""  + sentBy,
-			""  + sentTo
+		EnhancedPreparedStatement eps = new EnhancedPreparedStatement(
+		  	"insert into Messages " +
+		  	"values( default,     " +
+		  	"		 default,     " +
+		  	"		 <<subject>>, " +
+		  	"		 <<body>>,    " +
+		  	"		 <<sentBy>>,  " +
+		  	"		 <<sentTo>>   " +
+		  	");"
 		);
+		
+		try {
+			eps.set( "subject", subject );
+			eps.set( "body",    body    );
+			eps.set( "sentBy",  sentBy  );
+			eps.set( "sentTo",  sentTo  );
+			
+			eps.update();
+			
+			return eps.getGeneratedKey();
+		}
+		finally {
+			System.out.println( "finally" );
+			eps.close();
+		}
 	}
 	
 }
